ac37bd25d76a5605e4a3955770fac27c8de7cd52
Ali Kutluozen <ali_kutluozen@hotmail.com>
2017-09-29 08:55:22 -0400

    temporary login

--- backend/checklogin.php
+++ backend/checklogin.php
@@ -1,7 +1,8 @@
 <?php 
     $username = $_POST["username"];
     $password = $_POST["password"];
-    if($username !== "cvoadmin" || $password !== "cvoadmin"){
+    // TEMPORARY WAY TO GIVE ACCESS TO INTERNAL PEOPLE - Will be moved to a database system.
+    if($username !== "admin" || $password !== "admin"){
         header("Location: ../index.html#!/login.html");
     } else {
         setcookie("loggedin", "true", time() + (86400 * 30), "/"); // 86400 = 1 day
